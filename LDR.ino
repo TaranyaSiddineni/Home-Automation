1e1d01cbe9d434a9ecaee530ea1b309676ed8cb3
TaranyaSiddineni <taranyasiddineni@gmail.com>
2025-11-08 15:32:09 +0530

    Add files via upload

---
+++ LDR.ino
@@ -0,0 +1,27 @@
+//WAP to control the brightness of the led by reading values from LDR
+#define LDR  A0
+#define LED  3
+
+
+void setup() 
+{
+  // put your setup code here, to run once:
+  pinMode(3,OUTPUT);
+
+}
+
+unsigned int Light_intensity, duty;
+void loop()
+{
+  //read light intensity from  LDR sensor
+  Light_intensity = analogRead(LDR);//0 to 1023
+
+  //scale down the light intensity or map the values from 1 to 1023 to 225 to 0
+  duty = 255 - ( Light_intensity / 4);
+
+  //control brightness of the led
+  analogWrite(LED , duty);
+
+  delay(100);
+
+}
